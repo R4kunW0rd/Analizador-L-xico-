42e1bb80f1b6c7b6a85580976a71da33188f15da
R4kunW0rd <160672657+R4kunW0rd@users.noreply.github.com>
2025-10-02 07:54:13 -0600

    Add files via upload

---
+++ Analizador Lexico/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Analizador_Lexico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.clmToken = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmLinea = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmColumna = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnAnalizar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(12, 12);
+            this.txtCodigo.Multiline = true;
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtCodigo.Size = new System.Drawing.Size(782, 294);
+            this.txtCodigo.TabIndex = 0;
+            this.txtCodigo.WordWrap = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clmToken,
+            this.clmLinea,
+            this.clmColumna});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 323);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(514, 229);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // clmToken
+            // 
+            this.clmToken.HeaderText = "Token";
+            this.clmToken.MinimumWidth = 8;
+            this.clmToken.Name = "clmToken";
+            this.clmToken.ReadOnly = true;
+            this.clmToken.Width = 150;
+            // 
+            // clmLinea
+            // 
+            this.clmLinea.HeaderText = "Linea";
+            this.clmLinea.MinimumWidth = 8;
+            this.clmLinea.Name = "clmLinea";
+            this.clmLinea.ReadOnly = true;
+            this.clmLinea.Width = 150;
+            // 
+            // clmColumna
+            // 
+            this.clmColumna.HeaderText = "Columna";
+            this.clmColumna.MinimumWidth = 8;
+            this.clmColumna.Name = "clmColumna";
+            this.clmColumna.ReadOnly = true;
+            this.clmColumna.Width = 150;
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(584, 357);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(158, 67);
+            this.btnAbrir.TabIndex = 2;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // btnAnalizar
+            // 
+            this.btnAnalizar.Location = new System.Drawing.Point(584, 482);
+            this.btnAnalizar.Name = "btnAnalizar";
+            this.btnAnalizar.Size = new System.Drawing.Size(158, 70);
+            this.btnAnalizar.TabIndex = 3;
+            this.btnAnalizar.Text = "Analizar";
+            this.btnAnalizar.UseVisualStyleBackColor = true;
+            this.btnAnalizar.Click += new System.EventHandler(this.btnAnalizar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 591);
+            this.Controls.Add(this.btnAnalizar);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtCodigo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmToken;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmLinea;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmColumna;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnAnalizar;
+    }
+}
+
---
+++ Analizador Lexico/Form1.cs
@@ -0,0 +1,503 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Analizador_Lexico
+{
+    
+    public partial class Form1 : Form
+    {
+        private string codigoFuente = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Token
+        {
+            public string Tipo { get; set; }
+            public string Valor { get; set; }
+            public int Linea { get; set; }
+            public int Columna { get; set; }
+
+            public Token(string tipo, string valor, int linea, int columna)
+            {
+                Tipo = tipo;
+                Valor = valor;
+                Linea = linea;
+                Columna = columna;
+            }
+        }
+
+        public class AnalizadorLexicoC
+        {
+            private string codigoFuente;
+            private int posicion;
+            private int linea;
+            private int columna;
+            private StringBuilder errores;
+
+            // Palabras reservadas de C
+            private readonly HashSet<string> palabrasReservadas = new HashSet<string>
+        {
+            "auto", "break", "case", "char", "const", "continue", "default",
+            "do", "double", "else", "enum", "extern", "float", "for", "goto",
+            "if", "int", "long", "register", "return", "short", "signed",
+            "sizeof", "static", "struct", "switch", "typedef", "union",
+            "unsigned", "void", "volatile", "while"
+        };
+
+            // Operadores y símbolos
+            private readonly Dictionary<string, string> operadores = new Dictionary<string, string>
+        {
+            {"+", "OPERADOR_SUMA"},
+            {"-", "OPERADOR_RESTA"},
+            {"*", "OPERADOR_MULTIPLICACION"},
+            {"/", "OPERADOR_DIVISION"},
+            {"%", "OPERADOR_MODULO"},
+            {"=", "OPERADOR_ASIGNACION"},
+            {"==", "OPERADOR_IGUALDAD"},
+            {"!=", "OPERADOR_DIFERENTE"},
+            {"<", "OPERADOR_MENOR"},
+            {">", "OPERADOR_MAYOR"},
+            {"<=", "OPERADOR_MENOR_IGUAL"},
+            {">=", "OPERADOR_MAYOR_IGUAL"},
+            {"&&", "OPERADOR_AND"},
+            {"||", "OPERADOR_OR"},
+            {"!", "OPERADOR_NOT"},
+            {"&", "OPERADOR_BIT_AND"},
+            {"|", "OPERADOR_BIT_OR"},
+            {"^", "OPERADOR_BIT_XOR"},
+            {"~", "OPERADOR_BIT_NOT"},
+            {"<<", "OPERADOR_DESPLAZAMIENTO_IZQ"},
+            {">>", "OPERADOR_DESPLAZAMIENTO_DER"},
+            {"++", "OPERADOR_INCREMENTO"},
+            {"--", "OPERADOR_DECREMENTO"},
+            {"+=", "OPERADOR_SUMA_ASIGNACION"},
+            {"-=", "OPERADOR_RESTA_ASIGNACION"},
+            {"*=", "OPERADOR_MULTIPLICACION_ASIGNACION"},
+            {"/=", "OPERADOR_DIVISION_ASIGNACION"},
+            {"%=", "OPERADOR_MODULO_ASIGNACION"},
+            {"&=", "OPERADOR_AND_ASIGNACION"},
+            {"|=", "OPERADOR_OR_ASIGNACION"},
+            {"^=", "OPERADOR_XOR_ASIGNACION"},
+            {"<<=", "OPERADOR_DESPLAZAMIENTO_IZQ_ASIGNACION"},
+            {">>=", "OPERADOR_DESPLAZAMIENTO_DER_ASIGNACION"}
+        };
+
+            private readonly Dictionary<string, string> simbolos = new Dictionary<string, string>
+        {
+            {"(", "PARENTESIS_IZQ"},
+            {")", "PARENTESIS_DER"},
+            {"{", "LLAVE_IZQ"},
+            {"}", "LLAVE_DER"},
+            {"[", "CORCHETE_IZQ"},
+            {"]", "CORCHETE_DER"},
+            {";", "PUNTO_COMA"},
+            {",", "COMA"},
+            {".", "PUNTO"},
+            {":", "DOS_PUNTOS"},
+            {"?", "INTERROGACION"},
+            {"#", "NUMERAL"}
+        };
+
+            public int ErroresCount { get; private set; }
+
+            public AnalizadorLexicoC(string codigo)
+            {
+                codigoFuente = codigo;
+                posicion = 0;
+                linea = 1;
+                columna = 1;
+                errores = new StringBuilder();
+                ErroresCount = 0;
+            }
+
+            public (List<Token> Tokens, string Errores) Analizar()
+            {
+                List<Token> tokens = new List<Token>();
+                errores.Clear();
+                ErroresCount = 0;
+
+                while (posicion < codigoFuente.Length)
+                {
+                    char caracterActual = codigoFuente[posicion];
+
+                    if (char.IsWhiteSpace(caracterActual))
+                    {
+                        ManejarEspacios();
+                    }
+                    else if (caracterActual == '/' && posicion + 1 < codigoFuente.Length && codigoFuente[posicion + 1] == '/')
+                    {
+                        ManejarComentarioLinea();
+                    }
+                    else if (caracterActual == '/' && posicion + 1 < codigoFuente.Length && codigoFuente[posicion + 1] == '*')
+                    {
+                        ManejarComentarioBloque();
+                    }
+                    else if (char.IsLetter(caracterActual) || caracterActual == '_')
+                    {
+                        tokens.Add(ManejarIdentificador());
+                    }
+                    else if (char.IsDigit(caracterActual))
+                    {
+                        tokens.Add(ManejarNumero());
+                    }
+                    else if (caracterActual == '"')
+                    {
+                        tokens.Add(ManejarCadena());
+                    }
+                    else if (caracterActual == '\'')
+                    {
+                        tokens.Add(ManejarCaracter());
+                    }
+                    else
+                    {
+                        tokens.Add(ManejarOperadorOSimbolo());
+                    }
+                }
+
+                return (tokens, errores.ToString());
+            }
+
+            private void ManejarEspacios()
+            {
+                while (posicion < codigoFuente.Length && char.IsWhiteSpace(codigoFuente[posicion]))
+                {
+                    if (codigoFuente[posicion] == '\n')
+                    {
+                        linea++;
+                        columna = 1;
+                    }
+                    else
+                    {
+                        columna++;
+                    }
+                    posicion++;
+                }
+            }
+
+            private void ManejarComentarioLinea()
+            {
+                int lineaInicio = linea;
+                int columnaInicio = columna;
+
+                while (posicion < codigoFuente.Length && codigoFuente[posicion] != '\n')
+                {
+                    posicion++;
+                    columna++;
+                }
+            }
+
+            private void ManejarComentarioBloque()
+            {
+                int lineaInicio = linea;
+                int columnaInicio = columna;
+
+                posicion += 2; // Saltar "/*"
+                columna += 2;
+
+                while (posicion < codigoFuente.Length - 1)
+                {
+                    if (codigoFuente[posicion] == '*' && codigoFuente[posicion + 1] == '/')
+                    {
+                        posicion += 2;
+                        columna += 2;
+                        return;
+                    }
+
+                    if (codigoFuente[posicion] == '\n')
+                    {
+                        linea++;
+                        columna = 1;
+                    }
+                    else
+                    {
+                        columna++;
+                    }
+                    posicion++;
+                }
+
+                // Si llegamos aquí, el comentario no se cerró
+                AgregarError(lineaInicio, columnaInicio, "Comentario de bloque no cerrado");
+            }
+
+            private Token ManejarIdentificador()
+            {
+                int inicioLinea = linea;
+                int inicioColumna = columna;
+                StringBuilder lexema = new StringBuilder();
+
+                while (posicion < codigoFuente.Length &&
+                      (char.IsLetterOrDigit(codigoFuente[posicion]) || codigoFuente[posicion] == '_'))
+                {
+                    lexema.Append(codigoFuente[posicion]);
+                    posicion++;
+                    columna++;
+                }
+
+                string valor = lexema.ToString();
+                string tipo = palabrasReservadas.Contains(valor) ? "PALABRA_RESERVADA" : "IDENTIFICADOR";
+
+                return new Token(tipo, valor, inicioLinea, inicioColumna);
+            }
+
+            private Token ManejarNumero()
+            {
+                int inicioLinea = linea;
+                int inicioColumna = columna;
+                StringBuilder lexema = new StringBuilder();
+                bool tienePunto = false;
+
+                while (posicion < codigoFuente.Length &&
+                      (char.IsDigit(codigoFuente[posicion]) || codigoFuente[posicion] == '.'))
+                {
+                    if (codigoFuente[posicion] == '.')
+                    {
+                        if (tienePunto)
+                        {
+                            AgregarError(linea, columna, "Número con múltiples puntos decimales");
+                            break;
+                        }
+                        tienePunto = true;
+                    }
+
+                    lexema.Append(codigoFuente[posicion]);
+                    posicion++;
+                    columna++;
+                }
+
+                string tipo = tienePunto ? "NUMERO_REAL" : "NUMERO_ENTERO";
+                return new Token(tipo, lexema.ToString(), inicioLinea, inicioColumna);
+            }
+
+            private Token ManejarCadena()
+            {
+                int inicioLinea = linea;
+                int inicioColumna = columna;
+                StringBuilder lexema = new StringBuilder();
+
+                lexema.Append(codigoFuente[posicion]); // Agregar comilla inicial
+                posicion++;
+                columna++;
+
+                while (posicion < codigoFuente.Length && codigoFuente[posicion] != '"')
+                {
+                    if (codigoFuente[posicion] == '\\')
+                    {
+                        // Manejar secuencias de escape
+                        lexema.Append(codigoFuente[posicion]);
+                        posicion++;
+                        columna++;
+
+                        if (posicion < codigoFuente.Length)
+                        {
+                            lexema.Append(codigoFuente[posicion]);
+                            posicion++;
+                            columna++;
+                        }
+                    }
+                    else if (codigoFuente[posicion] == '\n')
+                    {
+                        AgregarError(linea, columna, "Cadena no cerrada en la misma línea");
+                        break;
+                    }
+                    else
+                    {
+                        lexema.Append(codigoFuente[posicion]);
+                        posicion++;
+                        columna++;
+                    }
+                }
+
+                if (posicion < codigoFuente.Length && codigoFuente[posicion] == '"')
+                {
+                    lexema.Append(codigoFuente[posicion]); // Agregar comilla final
+                    posicion++;
+                    columna++;
+                }
+                else
+                {
+                    AgregarError(inicioLinea, inicioColumna, "Cadena no cerrada");
+                }
+
+                return new Token("CADENA", lexema.ToString(), inicioLinea, inicioColumna);
+            }
+
+            private Token ManejarCaracter()
+            {
+                int inicioLinea = linea;
+                int inicioColumna = columna;
+                StringBuilder lexema = new StringBuilder();
+
+                lexema.Append(codigoFuente[posicion]); // Agregar comilla simple inicial
+                posicion++;
+                columna++;
+
+                while (posicion < codigoFuente.Length && codigoFuente[posicion] != '\'')
+                {
+                    if (codigoFuente[posicion] == '\\')
+                    {
+                        // Manejar secuencias de escape
+                        lexema.Append(codigoFuente[posicion]);
+                        posicion++;
+                        columna++;
+
+                        if (posicion < codigoFuente.Length)
+                        {
+                            lexema.Append(codigoFuente[posicion]);
+                            posicion++;
+                            columna++;
+                        }
+                    }
+                    else if (codigoFuente[posicion] == '\n')
+                    {
+                        AgregarError(linea, columna, "Carácter no cerrado en la misma línea");
+                        break;
+                    }
+                    else
+                    {
+                        lexema.Append(codigoFuente[posicion]);
+                        posicion++;
+                        columna++;
+                    }
+                }
+
+                if (posicion < codigoFuente.Length && codigoFuente[posicion] == '\'')
+                {
+                    lexema.Append(codigoFuente[posicion]); // Agregar comilla simple final
+                    posicion++;
+                    columna++;
+                }
+                else
+                {
+                    AgregarError(inicioLinea, inicioColumna, "Carácter no cerrado");
+                }
+
+                return new Token("CARACTER", lexema.ToString(), inicioLinea, inicioColumna);
+            }
+
+            private Token ManejarOperadorOSimbolo()
+            {
+                int inicioLinea = linea;
+                int inicioColumna = columna;
+
+                // Verificar operadores de 2 caracteres primero
+                if (posicion + 1 < codigoFuente.Length)
+                {
+                    string dosCaracteres = codigoFuente.Substring(posicion, 2);
+                    if (operadores.ContainsKey(dosCaracteres))
+                    {
+                        posicion += 2;
+                        columna += 2;
+                        return new Token(operadores[dosCaracteres], dosCaracteres, inicioLinea, inicioColumna);
+                    }
+                }
+
+                // Verificar operadores o símbolos de 1 carácter
+                string unCaracter = codigoFuente[posicion].ToString();
+                if (operadores.ContainsKey(unCaracter))
+                {
+                    posicion++;
+                    columna++;
+                    return new Token(operadores[unCaracter], unCaracter, inicioLinea, inicioColumna);
+                }
+                else if (simbolos.ContainsKey(unCaracter))
+                {
+                    posicion++;
+                    columna++;
+                    return new Token(simbolos[unCaracter], unCaracter, inicioLinea, inicioColumna);
+                }
+                else
+                {
+                    // Carácter no reconocido
+                    AgregarError(inicioLinea, inicioColumna, $"Carácter no reconocido: '{unCaracter}'");
+                    posicion++;
+                    columna++;
+                    return new Token("ERROR", unCaracter, inicioLinea, inicioColumna);
+                }
+            }
+
+            private void AgregarError(int line, int column, string mensaje)
+            {
+                errores.AppendLine($"Línea {line}, Columna {column}: {mensaje}");
+                ErroresCount++;
+            }
+        }
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Archivos C (*.c)|*.c|Todos los archivos (*.*)|*.*";
+            openFile.FilterIndex = 1;
+            openFile.RestoreDirectory = true;
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    codigoFuente = File.ReadAllText(openFile.FileName);
+                    txtCodigo.Text = codigoFuente;
+                    this.Text = $"Analizador Léxico - {Path.GetFileName(openFile.FileName)}";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error al leer el archivo: {ex.Message}",
+                                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnAnalizar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtCodigo.Text))
+            {
+                MessageBox.Show("Primero abre un archivo o escribe código.",
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            codigoFuente = txtCodigo.Text;
+
+            try
+            {
+                AnalizadorLexicoC analizador = new AnalizadorLexicoC(codigoFuente);
+                var (tokens, errores) = analizador.Analizar();
+
+                // Limpiar y mostrar resultados
+                dataGridView1.Rows.Clear();
+
+                int indice = 1;
+                foreach (var token in tokens)
+                {
+                    dataGridView1.Rows.Add(token.Tipo, token.Valor, token.Linea, token.Columna, indice++);
+                }
+
+                // Actualizar título
+                this.Text = $"Analizador Léxico - {tokens.Count} tokens, {analizador.ErroresCount} errores";
+
+                // Mostrar errores si existen
+                if (!string.IsNullOrEmpty(errores))
+                {
+                    MessageBox.Show("Errores encontrados:\n" + errores,
+                                  "Errores Léxicos", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error durante el análisis: {ex.Message}",
+                              "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
